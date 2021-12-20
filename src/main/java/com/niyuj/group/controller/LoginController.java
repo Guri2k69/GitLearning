2b616a57417f32b98327f6b4d3642f960ae127c9
Guri2k69 <gsingh.cse18@chitkarauniversity.edu.in>
2021-12-20 13:06:43 +0530

    added basic spring security by hardcoding usernamre and password in application.properties

--- src/main/java/com/niyuj/group/controller/LoginController.java
+++ src/main/java/com/niyuj/group/controller/LoginController.java
@@ -1,18 +1,17 @@
 package com.niyuj.group.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest/auth")
 public class LoginController {
-	@Autowired
-	ServiceLayer servicelayer;
-
-	@RequestMapping(method=RequestMethod.POST, value="/login")
-	public String loginApi() {
-		servicelayer.login();
-		return "Logged in Succsefuly";
+	
+	@GetMapping("/welcome")
+	public String welcomeapi() {
+		
+		return "welcome user!";
 	}
 }
