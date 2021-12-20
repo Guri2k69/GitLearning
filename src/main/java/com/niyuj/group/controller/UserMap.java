4db576394ab2ce881baf822769ec336b0ae87106
Guri2k69 <gsingh.cse18@chitkarauniversity.edu.in>
2021-12-20 18:02:30 +0530

    changed login api

--- src/main/java/com/niyuj/group/controller/UserMap.java
+++ src/main/java/com/niyuj/group/controller/UserMap.java
@@ -6,36 +6,31 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserMap {
-	
-	private static HashMap<String,String> map= new HashMap<String,String>();
 
-	public static HashMap<String, String> getMap() {
-		return map;
-	}
+	private static HashMap<String, String> map = new HashMap<String, String>();
+	private static HashMap<String, String> tokens = new HashMap<String, String>();
 
-	public static void setMap(HashMap<String, String> map) {
-		UserMap.map = map;
-	}
 	{
-	map.put("admin","admin");
-	map.put("user1", "password1");
-	map.put("user2", "password2");
-	map.put("user3", "password3");
-	map.put("user4", "password4");
-	map.put("user5", "password5");
-	map.put("user6", "password6");
-	map.put("user7", "password7");
-	map.put("user8", "password8");
-	map.put("user9", "password9");
-	map.put("user10", "password10");
+		map.put("admin", "admin");
+		map.put("user1", "password1");
+		map.put("user2", "password2");
+		map.put("user3", "password3");
+		map.put("user4", "password4");
+		map.put("user5", "password5");
+		map.put("user6", "password6");
+		map.put("user7", "password7");
+		map.put("user8", "password8");
+		map.put("user9", "password9");
+		map.put("user10", "password10");
 	}
-	public String login(String Username) {
-		if(map.containsKey(Username)) {
-			return Username+" "+"has looged in succefully";
+
+	public String login(String username, String password) {
+
+		if (map.containsKey(username) && (map.get(username).equals(password))) {
+			return "User looged in succefully";
+		} else {
+			return "wrong credentials entered";
 		}
-		else {
-			return "you have entered wrong credentials";
-		}
-		
+
 	}
 }
